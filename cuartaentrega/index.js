731495764c8f8a887c8c7c2ae305c766e3481244
Leonardo Carro <karrink22@gmail.com>
2022-12-15 15:18:07 -0300

    cuarto desafío reentrega

--- cuartaentrega/index.js
+++ cuartaentrega/index.js
@@ -1,77 +1,93 @@
 /*
-POST '/api/productos' -> recibe y agrega un producto, y lo devuelve con su id asignado.
 PUT '/api/productos/:id' -> recibe y actualiza un producto según su id.
 DELETE '/api/productos/:id' -> elimina un producto según su id.
 
--Cada producto estará representado por un objeto con el siguiente formato: {
-  title: (nombre del producto),
-  price: (precio),
-  thumbnail: (url al logo o foto del producto)
-}
+Cada producto estará representado por un objeto con el siguiente formato:
+Cada ítem almacenado dispondrá de un id numérico proporcionado por el backend, comenzando en 1, y que se irá incrementando a medida de que se incorporen productos. Ese id será utilizado para identificar un producto que va a ser listado en forma individual.
 
--Cada ítem almacenado dispondrá de un id numérico proporcionado por el backend, comenzando en 1, y que se irá incrementando a medida de que se incorporen productos. Ese id será utilizado para identificar un producto que va a ser listado en forma individual.
-
--Para el caso de que un producto no exista, se devolverá el objeto:
+Para el caso de que un producto no exista, se devolverá el objeto:
 { error : 'producto no encontrado' }
--Implementar la API en una clase separada, utilizando un array como soporte de persistencia en memoria.
--Incorporar el Router de express en la url base '/api/productos' y configurar todas las subrutas en base a este.
--Crear un espacio público de servidor que contenga un documento index.html con un formulario de ingreso de productos con los datos apropiados.
--El servidor debe estar basado en express y debe implementar los mensajes de conexión al puerto 8080 y en caso de error, representar la descripción del mismo.
--Las respuestas del servidor serán en formato JSON. La funcionalidad será probada a través de Postman y del formulario de ingreso.
+Implementar la API en una clase separada, utilizando un array como soporte de persistencia en memoria.
+Incorporar el Router de express en la url base '/api/productos' y configurar todas las subrutas en base a este.
+Crear un espacio público de servidor que contenga un documento index.html con un formulario de ingreso de productos con los datos apropiados.
+El servidor debe estar basado en express y debe implementar los mensajes de conexión al puerto 8080 y en caso de error, representar la descripción del mismo.
+Las respuestas del servidor serán en formato JSON. La funcionalidad será probada a través de Postman y del formulario de ingreso.
 
-*/
+/* */
 
 const express = require('express');
-const { Router } = express;
-
 const app = express();
+const productosRouter = express.Router();
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
+const PORT = 8080;
 
-const productos = [{
-  id: 1,
-  title: 'azucar',
-  price: 135,
-  thumbnail:'url/azucar'
-},{
-  id: 2,
-  title: 'harina',
-  price: 246,
-  thumbnail:'url/harina'
-},{
-  id: 3,
-  title: 'aceite',
-  price: 357,
-  thumbnail:'url/aceite'
-}];
 
-const productosRouter = Router();
+class Contenedor {
+    constructor() {
+        this.id = 1;
+        this.content = [];
+    }
 
-productosRouter.get('/productos', (req, res) => {
-  res.json(productos)
-});
+    save = (producto) => {
+        producto.id = this.id;
+        this.content.push(producto);
+        this.id ++;
 
-productosRouter.get('/productos/:id', (req, res) => {
-  let id = req.params.id
-  let productosid = productos.find(item => item.id == id)
-  res.json(productosid)
-});
+        return producto.id;
+    }
 
-productosRouter.get('/guardar', (req, res) => {
-  console.log(`I'm the route`);
-  res.json(productos)
-});
+    getById = (id) => {
+        const producto = this.content.find(element => element.id === id);
+        return producto ? producto : null;
+    }
+
+    getAll = () => this.content;
+
+    deleteById = (id) => {
+        const updatedContent = this.content.filter(element => element.id !== id);
+        this.content = updatedContent;
+    }
+
+    deleteAll = () => {
+        this.content = [];
+    }
+};
+
+const productosClass = new Contenedor();
 
 productosRouter.post('/guardar', (req, res) => {
-  console.log(req.body);
-  const productoToAdd = req.body;
-  productos.push(productoToAdd);
+    const productos = req.body;
+    const id = productosClass.save(productos);
 
-  res.status(200).json({ added: productoToAdd });
+    res.send(id.toString());
 });
 
+productosRouter.get('/', (req, res) => {
+    const productos = productosClass.getAll();
 
-app.use('/api', productosRouter);
-const PORT = 8080;
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+    res.json(productos);
+});
+
+productosRouter.get('/:Id', (req, res) => {
+    const productoId = req.params.productoId;
+    const productos = productosClass.getById(Number(productoId));
+
+    res.json(productos);
+});
+
+productosRouter.delete('/:Id', (req, res) => {
+    const productoId = req.params.productoId;
+    const productos = productosClass.deleteById(Number(productoId));
+// No logrado
+    res.json(productos);
+});
+
+productosRouter.put('/:Id', (req, res) => {
+// No logrado
+});
+
+app.use('/api/productos', productosRouter);
+const server = app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+server.on('error', err => console.log(`Error: ${err}`));
